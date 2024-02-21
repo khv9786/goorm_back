1dbc71fd7585213c9884c5da74c0aad87b9193cd
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-02-21 11:19:04 +0900

    [feat] product item id

--- Task09.ShoppingCart/src/Product.java
+++ Task09.ShoppingCart/src/Product.java
@@ -1,10 +1,12 @@
 import java.util.Objects;
 
 public class Product {
+    private long id;
     private String name;
     private int price;
 
     public Product(String name, int price) {
+        this.id = ++id;
         this.name = name;
         this.price = price;
     }
